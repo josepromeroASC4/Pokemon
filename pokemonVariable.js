39de1cae149bca22f2667f9a5fe51475171bc2ed
josepromeroASC4 <joeromero299@gmail.com>
2017-07-11 13:35:00 -0400

    first commit

---
+++ pokemonVariable.js
@@ -0,0 +1,12 @@
+var aggronHP = 98;
+var aggronNum = 306;
+var aggronType = "Steel and Rock";
+var aggronBestMove = "Thunder";
+var aggronLastEvo = true;
+
+aggronAttack = 50;
+aggronDefense = 70;
+aggronSpeed = 40;
+
+console.log(aggronAttack + 20);
+console.log(aggronAttack);
